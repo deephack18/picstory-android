26d7e0cb23fdd6afaa169957558875acce2670f5
hlibbabii <hlibbabii@gmail.com>
2018-11-16 23:09:25 +0100

    add http client draft

---
+++ app/src/main/java/com/anjandash/weventure/restclient/model/Challenge.java
@@ -0,0 +1,11 @@
+package com.anjandash.weventure.restclient.model;
+
+/**
+ * Created by hlib on 11/16/18.
+ */
+
+public class Challenge {
+    private int found;
+    private String image;
+    private int challengeId;
+}
