c98852557c729efa2e99c9ef3448b57b225e1ae8
Anjan <anjan8@gmail.com>
2018-11-17 05:40:03 +0100

    updates 5.39

--- app/src/main/java/com/anjandash/weventure/HPFActivity.java
+++ app/src/main/java/com/anjandash/weventure/HPFActivity.java
@@ -17,20 +17,27 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.anjandash.weventure.restclient.RequestSender;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 public class HPFActivity extends AppCompatActivity {
 
     public static final int REQUEST_IMAGE_CAPTURE = 1;
-    public static final int resultCode = 0;
+    //public static final int RESULT_OK = 0;
 
     Bitmap mainPhoto, jinxPhoto;
     Uri mainUri;
+
+    RequestSender requestSender;
 
 
     @Override
@@ -41,19 +48,18 @@ public class HPFActivity extends AppCompatActivity {
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
 
-        Button button = (Button) findViewById(R.id.buttontp);
+        Button button = (Button) findViewById(R.id.buttontp2);
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                    Boolean ch = true;
-                    if (ch) {
-                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        Context context = HPFActivity.this;
-                        mainUri = Uri.fromFile(new File(context.getExternalCacheDir().getPath(), "pickImageResult.jpg"));
 
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mainUri);
-                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                     }
+
 
             }
         });
@@ -62,81 +68,99 @@ public class HPFActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        Toast.makeText(HPFActivity.this, "bien!", Toast.LENGTH_SHORT);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            try {
-                if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
-                    mainPhoto = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mainUri);
-                    jinxPhoto = mainPhoto;
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
 
-                    //base64 encoded string
+            ImageView imv = (ImageView) findViewById(R.id.imageView);
+            imv.setImageBitmap(imageBitmap);
 
-//                    if(data.getData()==null){
-//                        bitmap = (Bitmap)data.getExtras().get("data");
-//                    }else{
-//                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream .toByteArray();
+
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+
+        }
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            try {
+//                if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+//                    mainPhoto = MediaStore.Images.Media.getBitmap(HPFActivity.this.getContentResolver(), mainUri);
+//                    jinxPhoto = mainPhoto;
+//
+//                    //base64 encoded string
+//
+//                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//                    jinxPhoto.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+//                    byte[] byteArray = byteArrayOutputStream .toByteArray();
+//
+//                    String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+//
+//                    ImageView imv = (ImageView) findViewById(R.id.imageView);
+//                    imv.setImageBitmap(jinxPhoto);
+//
+//
+//
+//                }
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
+
+//    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+//
+//    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+//            final Context context) {
+//        int currentAPIVersion = Build.VERSION.SDK_INT;
+//        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+//            if (ContextCompat.checkSelfPermission(context,
+//                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                        (Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE);
+//
+//                } else {
+//                    ActivityCompat
+//                            .requestPermissions(
+//                                    (Activity) context,
+//                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+//                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+//                }
+//                return false;
+//            } else {
+//                return true;
+//            }
+//
+//        } else {
+//            return true;
+//        }
+//    }
+//
+//    public void showDialog(final String msg, final Context context,
+//                           final String permission) {
+//        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+//        alertBuilder.setCancelable(true);
+//        alertBuilder.setTitle("Permission necessary");
+//        alertBuilder.setMessage(msg + " permission is necessary");
+//        alertBuilder.setPositiveButton(android.R.string.yes,
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        ActivityCompat.requestPermissions((Activity) context,
+//                                new String[] { permission },
+//                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
 //                    }
-
-
-                    Toast.makeText(HPFActivity.this, "success", Toast.LENGTH_SHORT);
-
-
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
-
-    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
-            final Context context) {
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(context,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        (Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-                } else {
-                    ActivityCompat
-                            .requestPermissions(
-                                    (Activity) context,
-                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
-                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                }
-                return false;
-            } else {
-                return true;
-            }
-
-        } else {
-            return true;
-        }
-    }
-
-    public void showDialog(final String msg, final Context context,
-                           final String permission) {
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
-        alertBuilder.setCancelable(true);
-        alertBuilder.setTitle("Permission necessary");
-        alertBuilder.setMessage(msg + " permission is necessary");
-        alertBuilder.setPositiveButton(android.R.string.yes,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions((Activity) context,
-                                new String[] { permission },
-                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                    }
-                });
-        AlertDialog alert = alertBuilder.create();
-        alert.show();
-    }
+//                });
+//        AlertDialog alert = alertBuilder.create();
+//        alert.show();
+//    }
 
 
 
--- app/src/main/java/com/anjandash/weventure/HistoricalPictureFoundActivity.java
+++ app/src/main/java/com/anjandash/weventure/HistoricalPictureFoundActivity.java
@@ -1,24 +1,31 @@
 package com.anjandash.weventure;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.anjandash.weventure.restclient.model.Challenge;
 import com.google.gson.Gson;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by hlib on 11/17/18.
  */
 
 public class HistoricalPictureFoundActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,44 @@ public class HistoricalPictureFoundActivity extends AppCompatActivity {
         Bitmap bitmap = getBitmapFromSharedPreferences();
         ImageView view = findViewById(R.id.hist_img);
         view.setImageBitmap(bitmap);
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        Button button = (Button) findViewById(R.id.buttontp);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            ImageView imv = (ImageView) findViewById(R.id.imageView);
+            imv.setImageBitmap(imageBitmap);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream .toByteArray();
+
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+
+        }
     }
 
     private Bitmap getBitmapFromSharedPreferences() {
