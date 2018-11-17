bfc84c55e5cbe5fdbe915755d12790f4cfd57ccb
hlibbabii <hlibbabii@gmail.com>
2018-11-17 04:08:52 +0100

    change icon in notification

--- app/src/main/java/com/anjandash/weventure/ProfileActivity.java
+++ app/src/main/java/com/anjandash/weventure/ProfileActivity.java
@@ -42,7 +42,7 @@ public class ProfileActivity extends AppCompatActivity {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.sc2)
+                .setSmallIcon(R.drawable.camera)
                 .setContentTitle("New historical photo")
                 .setContentText("Checkout a new historical photo and take a pic!")
                 .setContentIntent(pendingIntent)
