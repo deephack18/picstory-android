17279d002eca80d2bf1012c12fcbfbea393e42d1
hlibbabii <hlibbabii@gmail.com>
2018-11-17 03:22:24 +0100

    add historical picture found activity

---
+++ app/src/main/java/com/anjandash/weventure/HistoricalPictureFoundActivity.java
@@ -0,0 +1,16 @@
+package com.anjandash.weventure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hlib on 11/17/18.
+ */
+
+public class HistoricalPictureFoundActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historical_picture_found);
+    }
+}
